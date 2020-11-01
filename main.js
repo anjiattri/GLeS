699587a18aecf0c613322e20053948b37f1e7f8d
anjali singh <attri2707@gmail.com>
2020-11-01 20:31:27 +0530

    Added my project

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(".menu-toggler").on("click", function () {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+  $('nav a[href*="#"]').on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100,
+      },
+      2000
+    );
+  });
+});
+$("#up").on("click", function () {
+  $("html,body").animate(
+    {
+      scrollTop: 0,
+    },
+
+    2000
+  );
+});
+
+/*end loader*/
+// window.addEventListener("load", () => {
+//   const preload = document.querySelector(".loader");
+//   loader.classList.add("loader-hidden");
+// });
